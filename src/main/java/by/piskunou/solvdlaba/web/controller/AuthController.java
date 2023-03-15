508d9601df5466cff2142ba98a1f7bb44c2356da
cpiskunou solvd <cpiskunou@solvd.com>
2023-03-15 15:28:39 +0300

    make controller reactive

--- src/main/java/by/piskunou/solvdlaba/web/controller/AuthController.java
+++ src/main/java/by/piskunou/solvdlaba/web/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,23 +30,23 @@ public class AuthController {
     @PostMapping("/refresh")
     @Operation(summary = "Refresh jwt access token by valid refresh jwt token")
     @Parameter(name = "authEntityDTO", description = "Entity with access and refresh jwt token")
-    public AuthEntityDTO refresh(@RequestBody @Validated AuthEntityDTO authEntityDTO) {
+    public Mono<AuthEntityDTO> refresh(@RequestBody @Validated AuthEntityDTO authEntityDTO) {
         AuthEntity authEntity = authMapper.toEntity(authEntityDTO);
-        return authMapper.toDTO( authService.refresh(authEntity) );
+        return authService.refresh(authEntity).map(authMapper::toDTO);
     }
 
     @GetMapping("/password/new")
     @Operation(summary = "Forgot password: put in an email and get link to create new password")
     @Parameter(name = "email", description = "Email where to send a change link")
-    public void newPassword(@RequestParam("user_email") @Email(message = "Must be a well-formed email address") String email) {
-        authService.createPassword(email);
+    public Mono<Void> newPassword(@RequestParam("user_email") @Email(message = "Must be a well-formed email address") String email) {
+        return authService.createPassword(email);
     }
 
     @PostMapping("/password/edit")
     @Operation(summary = "Set up new password")
     @Parameter(name = "token", description = "one-time token to set new password")
-    public void editPassword(@RequestParam("reset_password_token") String token, @RequestBody @Validated(PasswordDTO.onEdit.class) PasswordDTO dto) {
-        authService.editPassword(token, passwordMapper.toEntity(dto));
+    public Mono<Void> editPassword(@RequestParam("reset_password_token") String token, @RequestBody @Validated(PasswordDTO.onEdit.class) PasswordDTO dto) {
+        return authService.editPassword(token, passwordMapper.toEntity(dto));
     }
 
 }
