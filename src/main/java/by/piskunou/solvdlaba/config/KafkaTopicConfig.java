0c5cd5157f51332797e676e54c6aa1941554d69b
solvd tpiskunou <tpiskunou@solvd.com>
2023-05-23 12:33:04 +0300

    removed comments

--- src/main/java/by/piskunou/solvdlaba/config/KafkaTopicConfig.java
+++ src/main/java/by/piskunou/solvdlaba/config/KafkaTopicConfig.java
@@ -8,16 +8,6 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 
-//    @Value(value = "${spring.kafka.bootstrap-servers}")
-//    private String bootstrapAddress;
-//
-//    @Bean
-//    public KafkaAdmin kafkaAdmin() {
-//        Map<String, Object> configs = new HashMap<>();
-//        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-//        return new KafkaAdmin(configs);
-//    }
-
     @Bean
     public NewTopic sendMailTopic() {
         return TopicBuilder.name("sendEmail")
